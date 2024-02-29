d41f9fe17c1d0486c07e11bd4a0189379c6690f2
local_ubuntu <kanikama775@gmail.com>
2024-02-29 21:29:16 +0900

    :construction: terraformモジュール追加                 - ansible実行用のssh鍵/キーペア

--- terraform/module/output.tf
+++ terraform/module/output.tf
@@ -1,5 +1,10 @@
-# SSH接続 コマンド
+# EC2 グローバルIP
 output "instance_public_ips" {
-  value = { for node in aws_instance.node : node.tags["Name"] => "ssh -i ${var.private_ssh_key_name} ubuntu@${node.public_ip}" }
+  value = { for node in aws_instance.node : node.tags["Name"] => "${node.public_ip}" }
+  description = "The public IP addresses of the instances."
+}
+# SSH接続 コマンド
+output "ssh_command" {
+  value = { for node in aws_instance.node : node.tags["Name"] => "ssh -i ${var.private_ssh_key_name} ${var.os_user}@${node.public_ip}" }
   description = "The public IP addresses of the instances."
 }
