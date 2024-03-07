70e90b9d4b161f1330f8f32c836a9a8d4398df23
Lamaglama39 <lamaglama621@gmail.com>
2024-03-07 23:12:24 +0900

    :recycle: terraform 環境ファイル修正             - インデント修正             - SSH認証鍵のフォルダパス修正

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -11,12 +11,12 @@ data "http" "ipv4_icanhazip" {
 locals {
   # common parameter
   ## タグ
-  pj    = "container"
-  env   = "terraform"
+  pj  = "container"
+  env = "terraform"
 
   tags = {
-    pj    = local.pj
-    env   = local.env
+    pj  = local.pj
+    env = local.env
   }
 
   # Network
@@ -35,14 +35,14 @@ locals {
 
   # EC2用キーペア
   ## SSHキー
-  private_ssh_key_name = ".ssh/ansible_key"
-  public_ssh_key_name = ".ssh/ansible_key.pub"
+  private_ssh_key_name = "ansible_key"
+  public_ssh_key_name  = "ansible_key.pub"
   ## キーペア
   key_pair_name = "key_pair_name"
 
   # SecurityGroup
   ## SSH接続用 送信元グローバルIP
-  current-ip = chomp(data.http.ipv4_icanhazip.body)
+  current-ip      = chomp(data.http.ipv4_icanhazip.body)
   allow_ssh_cidrs = ["${local.current-ip}/32"]
 
 }
@@ -51,7 +51,7 @@ module "main" {
   source = "../module"
 
   # tag
-  base_name = "${local.pj}"
+  base_name = local.pj
   tags      = local.tags
 
   # Network
@@ -59,13 +59,13 @@ module "main" {
   subnet_cidr = local.subnet_cidr
 
   # EC2
-  os_user = local.os_user
-  nodes         = local.nodes
-  instance_type = local.instance_type
-  private_ssh_key_name       = local.private_ssh_key_name
-  public_ssh_key_name       = local.public_ssh_key_name
-  key_pair_name      = local.key_pair_name
+  os_user              = local.os_user
+  nodes                = local.nodes
+  instance_type        = local.instance_type
+  private_ssh_key_name = local.private_ssh_key_name
+  public_ssh_key_name  = local.public_ssh_key_name
+  key_pair_name        = local.key_pair_name
 
-  allow_ssh_cidrs        = local.allow_ssh_cidrs
+  allow_ssh_cidrs = local.allow_ssh_cidrs
 
 }
